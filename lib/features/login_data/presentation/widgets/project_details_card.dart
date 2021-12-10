8722d697f0c6c0d8258c9219845671bcd2a78200
ahel-men <ahel-men@student.1337.ma>
2021-12-10 19:04:20 +0100

    fixing projects status crush

--- lib/features/login_data/presentation/widgets/project_details_card.dart
+++ lib/features/login_data/presentation/widgets/project_details_card.dart
@@ -4,13 +4,6 @@ import 'package:swifty/core/theme/GlobalTheme.dart';
 import 'package:swifty/features/login_data/data/model/project_details_model.dart';
 import 'package:swifty/features/login_data/presentation/bloc/login_data_bloc.dart';
 
-class ProjectStatus {
-  final FaIcon icon;
-  final Color color;
-
-  ProjectStatus({this.icon, this.color});
-}
-
 class ProjectDetailsCard extends StatelessWidget {
   final ProjectDetailsModel managedCursus;
   ProjectDetailsCard({Key key, this.managedCursus}) : super(key: key);
@@ -23,21 +16,17 @@ class ProjectDetailsCard extends StatelessWidget {
         : managedCursus.final_mark.toString() + "%";
     print(managedCursus.validated.toString());
     print(managedCursus.project.slug.toString());
-    final Map<String, ProjectStatus> projectStatusMap = {
-      "in_progress": ProjectStatus(
-          icon: FaIcon(FontAwesomeIcons.spinner, color: Colors.cyan[300]),
-          color: Colors.cyan[300]),
-      "waiting_for_correction": ProjectStatus(
-          icon: FaIcon(FontAwesomeIcons.spinner, color: Colors.cyan[300]),
-          color: Colors.cyan[300]),
-      "finished": managedCursus.validated == null ||
-              managedCursus.validated == false
-          ? ProjectStatus(
-              icon: FaIcon(FontAwesomeIcons.times, color: Colors.red[300]),
-              color: Colors.red[300])
-          : ProjectStatus(
-              icon: FaIcon(FontAwesomeIcons.check, color: Colors.green[300]),
-              color: Colors.green[300])
+    final Map<String, FaIcon> projectStatusMap = {
+      "in_progress": FaIcon(FontAwesomeIcons.spinner, color: Colors.cyan[300]),
+      "waiting_for_correction":
+          FaIcon(FontAwesomeIcons.spinner, color: Colors.cyan[300]),
+      "parent": FaIcon(FontAwesomeIcons.question, color: Colors.white),
+      "finished":
+          managedCursus.validated == null || managedCursus.validated == false
+              ? FaIcon(FontAwesomeIcons.times, color: Colors.red[300])
+              : FaIcon(FontAwesomeIcons.check, color: Colors.green[300]),
+      "creating_group":
+          FaIcon(FontAwesomeIcons.spinner, color: Colors.cyan[300]),
     };
 
     return Container(
@@ -59,7 +48,7 @@ class ProjectDetailsCard extends StatelessWidget {
           Expanded(
               child: Text(finalMark, style: TextStyle(color: Colors.white))),
           Expanded(
-            child: projectStatusMap[managedCursus.status].icon,
+            child: projectStatusMap[managedCursus.status],
           )
         ],
       ),
